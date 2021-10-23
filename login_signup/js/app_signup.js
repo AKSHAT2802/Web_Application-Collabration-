534ead472fff3b2759aebe9799db1048ed456cec
Karan Rajpara <karan.rajpara107598@marwadiuniversity.ac.in>
2021-10-23 18:09:15 +0530

    Signup data saved at firestore

--- login_signup/js/app_signup.js
+++ login_signup/js/app_signup.js
@@ -5,11 +5,17 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var flag = 0;
 const firebaseConfig = {
   apiKey: "AIzaSyB8Y7OU15I_IyIomfTOEXL0MVsI8Pcbd5g",
   authDomain: "gearup-d7328.firebaseapp.com",
@@ -25,27 +31,53 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
+const db = getFirestore(app);
+
 document.getElementById("btn_submit").addEventListener("click", function () {
   const email = document.getElementById("Email").value;
   const password = document.getElementById("Password").value;
+  const userName = document.getElementById("user_name").value;
+  const phnNo = document.getElementById("phn_no").value;
 
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
       // ...
-      alert("User created successfully");
+
       console.log("User added in the authentication");
+      alert("User created successfully");
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       // ..
       console.log(errorCode + errorMessage);
-
-      if(errorCode + errorMessage == "auth/email-already-in-useFirebase: Error (auth/email-already-in-use).")
-      {
+      flag = 1;
+      if (
+        errorCode + errorMessage ==
+        "auth/email-already-in-useFirebase: Error (auth/email-already-in-use)."
+      ) {
         alert("Account already exist with given email");
+        flag = 2;
       }
     });
+    console.log(flag);
+  // if no error
+  if (flag == 0) {
+    // adding document of new faculty member in firestore
+    // Add a new document in collection "faculty_info"
+    setDoc(doc(db, "faculty_info", email), {
+      F_Email: email,
+      F_Name: userName,
+      F_PhoneNo: phnNo,
+    });
+    //  setDoc(doc(db, "cities", "LA"), {
+    //   name: "Los Angeles",
+    //   state: "CA",
+    //   country: "USA"
+    // });
+
+    console.log("User added in the firestore");
+  }
 });
