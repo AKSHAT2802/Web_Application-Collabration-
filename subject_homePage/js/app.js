3058b239a8982f0d8b9e4985fb316b6b8417f098
Akshat Kakkad <akshat.kakkad2802@gmail.com>
2021-10-29 02:07:22 +0530

    Add Questions with excel redirected from subject homepage

--- subject_homePage/js/app.js
+++ subject_homePage/js/app.js
@@ -3,7 +3,9 @@ const name = params.get("subject");
 document.getElementById("ADM").addEventListener("click",function(){
     window.location.href="../../add_question/Manual/add_question.html?subject="+name;
 });
-
+document.getElementById("ADE").addEventListener("click",function(){
+    window.location.href="../../add_question/Excel/add_question_excel.html?subject="+name;
+});
 document.getElementById("create_quiz").addEventListener("click",function(){
     window.location.href="../../Create_Quiz/CreateQuiz.html?subject="+name;
 });
